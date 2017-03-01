2dd444bb3dad3e3700a5501a2e8e1ca8edef0da8
QingwenHe <ayxchily@163.com>
2017-03-01 20:05:26 +0800

    update

--- 5-algorithms/2-学设计模式.java
+++ 5-algorithms/2-学设计模式.java
@@ -1,18 +1,20 @@
-《适配器模式》
 
+/**
+ * 适配器模式
+ */
 //1、定义顶层接口
 public interface ClassName1{
-  public void func1();
+    public void func1();
 }
 //2、定义实现抽象类
 public ststract class ClassName2{
-  public void func2(){
-  }
+    public void func2(){
+    }
 }
 //3、定义实现类，继承抽象类，并实现接口
 public class ClasName3 extends ClassName2 implements ClassName1{
-    public fun1(){    //实现接口函数
-      fun2();             //调用抽象类函数
+    public fun1(){                  //实现接口函数
+        fun2();                         //调用抽象类函数
     }
 }
           
