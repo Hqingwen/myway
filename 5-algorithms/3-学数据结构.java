fce6c2b786978e5d6e8a6afdc66bad35d41ac557
QingwenHe <ayxchily@163.com>
2017-03-30 17:25:56 +0800

    update

--- 5-algorithms/3-学数据结构.java
+++ 5-algorithms/3-学数据结构.java
@@ -1,20 +1,122 @@
+/**
+ * 节点类(带后置节点）
+ * @author HQW
+ * @version 0.0.1
+ */
+public class Node{
 
-//堆栈
+    private Object data;
+    private Node next;
+
+    public Node(){
+        this.data = null;
+        this.next = null;
+    }
+    public Node(Object data,Node next){
+        this.data = data;
+        this.next = next;
+    }
+
+    public void setData(Object data){
+        this.data = data;
+    }
+    public Object getData(){
+        return this.data;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
+    public Node getNext(){
+        return this.next;
+    }
+
+    public String toString(){
+        rerturn "[\"data\":"+this.data.toString()+",\"next\":"+this.next+"]";
+    }
+}
+
+/**
+ * 节点类（带前置后置节点）
+ * @author HQW
+ * @version 0.0.1
+ */
+public class DNode(){
+
+    private Object data;
+    private DNode prev;
+    private DNode next;
+
+    public DNode(){
+        this.data = null;
+        this.prev = null;
+        this.next = null;
+    }
+    public DNode(Object data, DNode prev, DNode next){
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public void setData(Object data){
+        this.data = data;
+    }
+    public Object getData(){
+        return this.data;
+    }
+    public void setPrev(DNode prev){
+        this.prev = prev;
+    }
+    public DNode getPrev(){
+        return this.prev;
+    }
+    public void setNext(DNode next){
+        this.next = next;
+    }
+    public Dnode getNext(){
+        return this,next;
+    }
+
+    public String toString(){
+        rerturn "{\"data\":["+this.data.toString()+"], \"prev\":["+this.next++"], \"next\":["+this.next+"]}";
+    }
+}
+
+/**
+ * 单链表
+ * @author HQW
+ * @version 0.0.1
+ */
+public class NodeList
+
+
+
+
+
+/**
+ * 栈
+ * @author HQW
+ * @version 0.0.1
+ */
 public class Stack {
-    int[] data;
+
+    Object[] data;
+
     int maxSize;
+
     int top;
+
     public Stack(int maxSize) {
         this.maxSize = maxSize;
-        data = new int[maxSize];
+        data = new Object[maxSize];
         top = -1;
     }
+
     /**
     * 依次加入数据
     * @param data 要加入的数据
     * @return 添加是否成功
     */
-    public boolean push(int data) {
+    public boolean push(Object data) {
         if(top+1== maxSize) {
             System.out.println("栈已满!");
             return false;
@@ -22,14 +124,15 @@ public class Stack {
         this.data[++top] = data;
         return true;
     }
+
     /**
     * 从栈中取出数据
     * @return 取出的数据
     */
-    public int pop() throws Exception{
+    public Object pop() throws Exception{
         if(top==-1){
             throw new Exception("栈已空!");
         }
-    return this.data[top--];
+        return this.data[top--];
     }
 }
